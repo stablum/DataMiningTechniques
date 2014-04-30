715141fff1ea6a522888b974044d7584fad7ba17
Francesco Stablum <stablum@gmail.com>
2014-04-30 22:02:50 +0200

    DictReader

--- dataset_reader.py
+++ dataset_reader.py
@@ -1,4 +1,6 @@
-# load dataset
+# create reader for the dataset
+# please note: we cannot store it entirely in memory,
+# hence we need to read every time the data from disk
 
 import csv
 
@@ -6,13 +8,14 @@ FILENAME="data/training_set_VU_DM_2014.csv"
 
 def create_reader(filename):
 	file_obj = open(filename)
-	reader = csv.reader(file_obj)
+	reader = csv.DictReader(file_obj)
 	return reader
 
 def main():
 	print "inside __main__"
 	global FILENAME
-	reader = load(FILENAME)
+	reader = create_reader(FILENAME)
+	print reader.fieldnames
 
 if __name__ == "__main__":
 	main()
