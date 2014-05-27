b4e50bbab6059f29e869770cf924fadeea47dbc5
Tommy Wang <tjbwyk@gmail.com>
2014-05-27 19:45:23 +0200

    revise pandas2ranklib scripts.

--- pd2ranklib.py
+++ pd2ranklib.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import numpy as np
 import re
+import sys
+import os
+import string
 
-data = pd.read_csv('data/train_sel.csv')
+print('PLZ input the filename of the input data:')
+datafilename = sys.stdin.readline()
+datafilename = datafilename.strip()
+print('PLZ input the filename of the feature list:')
+featurefilename = sys.stdin.readline()
+featurefilename = featurefilename.strip()
+
+data = pd.read_csv(datafilename)
+features = pd.read_csv(featurefilename).columns.tolist()
+
+data = data[features]
 data = data.fillna(data.mean())
 
 data['rel'] = 0
@@ -12,7 +25,6 @@ data.loc[data['booking_bool'] == 1, 'rel'] = 5
 cols = data.columns.tolist()
 
 cols.remove('rel')
-cols.remove('gross_bookings_usd')
 cols.remove('booking_bool')
 cols.remove('click_bool')
 cols.remove('srch_id')
@@ -32,12 +44,9 @@ cols = ['rel', 'srch_id'] + cols
 
 length = data.shape[0] - 1
 
-f = open('data/train.txt', 'w')
-#for i in range(0, length):
-#	row = data.iloc[i].to_frame().T.to_string(columns = cols, formatters = formats_dict, index = False, header = False)
-#	f.write(row + '\n')
-#	print(str(i / length * 100) + '%, ' + str(i))
 buf = data.to_string(columns = cols, formatters = formats_dict, index = False, header = False)
 buf = re.sub(' +', ' ', buf)
+outfilename = datafilename.replace('_newfeature.txt', '_ranklib.txt')
+f = open(outfilename, 'w')
 f.write(buf)
 f.close()
