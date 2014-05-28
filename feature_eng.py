179dda96a254bad356a284dbdc2f6afb49a45225
Tommy Wang <tjbwyk@gmail.com>
2014-05-28 15:44:12 +0200

    comments for the feature_eng.py

--- feature_eng.py
+++ feature_eng.py
@@ -3,23 +3,32 @@ import numpy as np
 import sys
 import os
 
-#sys.stdout.write('PLZ input the filename of the raw data:')
+# Read the filename of the raw training data
 print('PLZ input the filename of the raw data:')
 infile = sys.stdin.readline()
 infile = infile.strip() 
+
+# Read the raw training data
 data = pd.read_csv(infile)
 
+# Transform the date_time format so that pandas can recognize 
 data['date_time'] = pd.to_datetime(data['date_time'])
 date_time = data['date_time']
 
+# Seperate the YEAR, MONTH, DAY and TIME(seconds in a day)
 data['year'] = [t.year for t in date_time]
 data['month'] = [t.month for t in date_time]
 data['day'] = [t.day for t in date_time]
 data['time_in_second'] = [t.hour * 3600 + t.minute * 60 + t.second for t in date_time]
 
+# Generate the filename for saving the data with new feature
 outfile_data = os.path.splitext(infile)[0] + '_newfeature.txt'
+
+# Generate the filename for saving the names of features(columns)
 outfile_flist = os.path.splitext(infile)[0] + '_featurelist.txt'
 
+# Save the data with new features
 data.to_csv(outfile_data, index = False)
 
+# Save the names of new features
 data.columns.tofile(outfile_flist, sep = ',')
--- train.bat
+++ train.bat
@@ -1,6 +1,6 @@
 @echo off
 set @stime=%time%
-java -Xms1000m -Xmx6000m -jar D:\Development\RankLib\bin\RankLib.jar -train data/training_set_VU_DM_2014_ranklib.txt -ranker 6 -metric2t NDCG@38 -tvs 0.8 -save LambdaMART_Single\model.txt -norm linear
+java -Xms1000m -Xmx6000m -jar D:\Development\RankLib\bin\RankLib.jar -train data/training_set_VU_DM_2014_ranklib.txt -ranker 6 -metric2t NDCG@38 -kcv 10 -kcvmd data/LambdaMART_10_fold_new -save LambdaMART_Single\model.txt -norm linear
 set @etime=%time%
 echo %@stime%
 echo %@etime%
