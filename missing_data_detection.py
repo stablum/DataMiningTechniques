6fee53e8eba6a4bd87a8ce2020568bbff02cdeaa
stablum@gmail.com <stablum@gmail.com>
2014-05-28 16:41:19 +0200

    detection of missing data and adding attributes
    detection of missing data and adding boolean attributes which are set as
    1 (data present) or 0 (data missing) of the respective column of
    reference.

---
+++ missing_data_detection.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# detects missing data in the input pandas dataset
+
+def run(dataset):
+    dataset_value_missing = pd.isnull(dataset)
+    for column in dataset_value_missing.columns:
+        if True in set(dataset_value_missing[column].values):
+            print "."
+            dataset[column+"_missing"] = dataset_value_missing[column].astype(int)
